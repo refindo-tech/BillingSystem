ec40854b646929ed71627f91bf2dc23f68905d35
Hernando Dio Palma <hernando.diopalma@gmail.com>
2024-08-30 01:57:31 +0700

    Add UI feature components for homepage

---
+++ resources/views/components/ui/feature.blade.php
@@ -0,0 +1,16 @@
+
+{{-- <section class="bg-white dark:bg-gray-900">
+    <div class="pt-8 px-4 mx-auto max-w-screen-xl lg:pt-16 lg:px-6">
+        <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+                Internet Murah, Stabil dan Unlimited
+            </h2>
+            <p class="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">
+                Fitur-Fitur berikut ini yang akan Anda dapatkan :
+            </p>
+        </div>
+    </div>
+</section> --}}
+
+<x-ui.feature1/>
+{{-- <x-ui.feature2/> --}}
---
+++ resources/views/components/ui/feature1.blade.php
@@ -0,0 +1,78 @@
+<section class="bg-gray-50 dark:bg-gray-800">
+    <div class="max-w-screen-xl px-4 py-8 mx-auto space-y-12 lg:space-y-20 lg:py-24 lg:px-6">
+        <!-- Row -->
+        <div class="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
+            <div class="text-gray-500 sm:text-lg dark:text-gray-400">
+                <h2 class="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600">Dukungan Tak Tertandingi, Kapan Saja, Di Mana Saja!</h2>
+                
+                
+                
+                <p class="mb-8 font-light lg:text-xl">Nikmati ketenangan dengan dukungan kami yang siap 24/7. Tim kami yang berdedikasi selalu siap mengatasi masalah Anda, di mana pun Anda berada. <br/> Dengan jangkauan area yang kuat, kami memastikan layanan lebih cepat dan konektivitas yang andal, menjamin Anda tetap terhubung saat paling dibutuhkan.</p>
+                <!-- List -->
+                <div class="w-full px-4 mx-auto text-center lg:px-6 pb-8">
+                    <dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
+                        <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold"><i class="bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600 fa-solid fa-user-clock"></i></dt>
+                            <dd class="font-normal text-gray-700 dark:text-gray-400">24/7 Support</dd>
+                        </div>
+                        <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold"><i class="bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600 fa-regular fa-map"></i></dt>
+                            <dd class="font-normal text-gray-700 dark:text-gray-400">Coverage Area</dd>
+                        </div>
+                        <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold"><i class="bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600 fa-solid fa-shield-halved"></i></dt>
+                            <dd class="font-normal text-gray-700 dark:text-gray-400">Garansi Kualitas Layanan</dd>
+                        </div>
+                    </dl>
+                </div>
+                <p class="mb-8 font-normal lg:text-xl">Percayakan kebutuhan internet Anda pada kami, dan rasakan perbedaannya!</p>
+            </div>
+            <img class="hidden h-[480px] object-fit w-full mb-4 rounded-lg lg:mb-0 lg:flex" src="{{ asset('images/tech.png') }}" alt="dashboard feature image">
+        </div>
+        <!-- Row -->
+        <div class="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
+            <img class="hidden h-[480px] object-fit w-full mb-4 rounded-lg lg:mb-0 lg:flex" src="{{ asset('images/tech2.png') }}" alt="feature image 2">
+            <div class="text-gray-500 sm:text-lg dark:text-gray-400">
+                <h2 class="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600">Teknologi Terdepan, Konektivitas Tanpa Batas!</h2>
+                <p class="mb-8 font-light lg:text-xl">Tetap unggul dengan platform kelas atas dan paket data tanpa batas dari kami. Teknologi fiber dan wireless kami menghadirkan kecepatan super cepat dan cakupan luas, memastikan Anda menikmati pengalaman internet terbaik tanpa batasan. Sambut masa depan konektivitas bersama RLNet.</p>
+                <!-- List -->
+                {{-- <ul role="list" class="pt-8 space-y-5 border-t border-gray-200 my-7 dark:border-gray-700">
+                    <li class="flex space-x-3">
+                        <!-- Icon -->
+                        <svg class="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                        <span class="text-base font-medium leading-tight text-gray-900 dark:text-white">Pilihan Platform Terbaik</span>
+                    </li>
+                    <li class="flex space-x-3">
+                        <!-- Icon -->
+                        <svg class="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                        <span class="text-base font-medium leading-tight text-gray-900 dark:text-white">Unlimited, Tanpa Batas Quota!</span>
+                    </li>
+                    <li class="flex space-x-3">
+                        <!-- Icon -->
+                        <svg class="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                        <span class="text-base font-medium leading-tight text-gray-900 dark:text-white">Media Fiber & Wireless</span>
+
+                </ul> --}}
+
+                <div class="w-full px-4 mx-auto text-center lg:px-6 pb-8">
+                    <dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
+                        <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold"><i class="bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600 fa-solid fa-server"></i></dt>
+                            <dd class="font-normal text-gray-700 dark:text-gray-400 ">Pilihan Platform Terbaik</dd>
+                        </div>
+                        <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold"><i class="bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600 fa-regular fa-credit-card"></i></dt>
+                            <dd class="font-normal text-gray-700 dark:text-gray-400">Unlimited, <br/>Tanpa Batas Quota!</dd>
+                        </div>
+                        <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold"><i class="bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600 fa-solid fa-link"></i></dt>
+                            <dd class="font-normal text-gray-700 dark:text-gray-400">Media Fiber & Wireless</dd>
+                        </div>
+                    </dl>
+                </div>
+
+                <p class="font-normal lg:text-xl">Bergabunglah dengan kami, dan nikmati kebebasan berselancar tanpa batas!</p>
+            </div>
+        </div>
+    </div>
+</section>
