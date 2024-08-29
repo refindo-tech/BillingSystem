e58d525c07022ec7f68b53615abc1c9eb38f27d0
Hernando Dio Palma <hernando.diopalma@gmail.com>
2024-08-30 01:58:23 +0700

    Add some component to homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,18 @@
 <x-layout.app title="Home">
-    <div class="container mx-auto mt-8">
-        <h1 class="text-3xl font-bold">Welcome</h1>
-    </div>
+    <x-ui.hero title="Saatnya menikmati layanan akses internet super cepat!!" description="Menjangkau Semua Kalangan Masyarakat Untuk Mendapatkan Akses Internet Hebat, Harga Hemat.">
+        <x-slot name="image">
+            <img src="{{ asset('images/hero/hero.png') }}" alt="hero">
+        </x-slot>
+    </x-ui.hero>
+    <x-ui.package title="Sensasi internetan ngebut seharian!" description="Layanan ini cocok buat kamu yang hobi streaming, hiburan keluarga dan juga untuk kerjaan online. Pilih Layananmu, Sesuaikan kebutuhanmu. Daftar Sekarang!"/>
+    <x-ui.service />
+    <x-ui.testimony />
+    <x-ui.hero2/>
+
+    <x-ui.feature/>
+
+    <x-ui.partner />
+
+    <x-ui.contact />
+
 </x-layout.app>
