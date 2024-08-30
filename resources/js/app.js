f00759520c8cdaf608002aff59a2626e939f8dc7
Hernando Dio Palma <hernando.diopalma@gmail.com>
2024-08-30 22:00:37 +0700

    Add smooth scroll to anchor in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,13 @@
 import './bootstrap';
 import 'flowbite';
+
+//universal smooth scroll to anchor
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
