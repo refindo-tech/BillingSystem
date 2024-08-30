12e72ef11ed4b1cb443ea3e6f3997220b7cc7bff
Hernando Dio Palma <hernando.diopalma@gmail.com>
2024-08-30 22:01:27 +0700

    Update contact section

--- resources/views/components/ui/contact.blade.php
+++ resources/views/components/ui/contact.blade.php
@@ -1,36 +1,115 @@
-<section class="bg-gray-100 dark:bg-gray-900">
+<section class="bg-gray-100 dark:bg-gray-900 relative" id="contact">
     <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-        <img class="w-full object-cover rounded-full" src="{{ asset('images/contact.png') }}" alt="dashboard image">
+        <img class="w-full object-cover rounded-xl" src="{{ asset('images/contact.png') }}" alt="dashboard image">
         {{-- <img class="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image"> --}}
         <div class="mt-4 md:mt-0">
-            {{-- <h3 class="mb-2 text-2xl tracking-tight font-bold text-gray-700 dark:text-white">PT Yasmin Amanah Media</h3> --}}
+            <div class="">
+                {{-- <h3 class="mb-2 text-2xl tracking-tight font-bold text-gray-700 dark:text-white">PT Yasmin Amanah Media</h3> --}}
             <h1 class="mb-4 text-6xl tracking-tight font-extrabold text-gray-900 dark:text-white bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600">
                 Mau Internet Yang Stabil ?
             </h1>
             <h4 class="text-lg tracking-tight font-bold text-gray-900 dark:text-white">Kemitraan dengan Dukungan Teknologi Terdepan</h4>
-            <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Jika Anda tertarik dengan layanan kami atau ingin bekerja sama dalam kemitraan jual kembali jasa layanan akses internet, kami siap mendukung Anda dengan teknologi canggih. Kami menyediakan dashboard aplikasi yang terintegrasi dengan WhatsApp Gateway, Payment Gateway, API Mikrotik, dan fitur-fitur lainnya yang mempermudah pengelolaan layanan. Dengan aplikasi ini, alur kerja Anda menjadi lebih terstruktur dan efektif.</p>
+            <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Jika Anda tertarik dengan layanan kami, atau ingin <strong class="font-bold">Kerjasama Kemitraan</strong> jual kembali jasa layanan akses internet, kami siap mendukung Anda dengan menyediakan dashboard aplikasi yang terintegrasi dengan WhatsApp Gateway, Payment Gateway, API Mikrotik, dan fitur-fitur lainnya yang mempermudah pengelolaan layanan anda.</p>
 
             <h4 class="text-lg tracking-tight font-bold text-gray-900 dark:text-white">Dukungan Menyeluruh dan Harga Terbaik</h4>
-            <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Selain teknologi dan support system yang handal, kami juga menawarkan harga terbaik dan pelatihan menyeluruh untuk Anda dan tim. Fitur-fitur seperti perhitungan komisi sales dan manajemen karyawan membuat pekerjaan Anda semakin efisien. Mari kita diskusikan potensi kemitraan ini dan bersama-sama meraih kesuksesan!</p>
+            <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Nikmati harga terbaik dan pelatihan lengkap untuk Anda dan tim. Fitur seperti perhitungan komisi sales dan manajemen karyawan akan memastikan pekerjaan anda menjadi lebih efisien. Diskusikan peluang ini dengan kami, dan raih kesuksesan bersama!</p>
 
-            <div class="w-full px-4 mx-auto text-center lg:px-6">
-                <dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white divide-x-2 divide-gray-500 divide-dashed">
-                    <div class="flex flex-col items-center justify-center">
-                        <dt class="mb-2 text-3xl md:text-3xl font-bold">8,113</dt>
-                        <dd class="font-normal text-gray-700 dark:text-gray-400">Happy Clients</dd>
-                    </div>
-                    <div class="flex flex-col items-center justify-center">
-                        <dt class="mb-2 text-3xl md:text-3xl font-bold">12,000</dt>
-                        <dd class="font-normal text-gray-700 dark:text-gray-400">Home Pass</dd>
-                    </div>
-                    <div class="flex flex-col items-center justify-center">
-                        <dt class="mb-2 text-3xl md:text-3xl font-bold">36</dt>
-                        <dd class="font-normal text-gray-700 dark:text-gray-400">Mitra RlNet</dd>
-                    </div>
-                </dl>
+            {{-- Kantor pusat --}}
+            
+
             </div>
+
+            <div class="w-full px-4 mx-auto lg:px-6 pb-4 border-4 dark:border-gray-700 rounded-xl border-dashed border-gray-300">
+                <div class="flex flex-col items-center justify-center w-full px-4 mx-auto lg:px-6 p-4 border-b-2 border-gray-300 dark:border-gray-700">
+                    <div class="mb-2 text-3xl md:text-3xl font-bold justify-start">
+                        <i class="bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600 fa-solid fa-location-dot"></i>
+                    </div>
+                    <h4 class="mb-0 text-2xl font-bold text-gray-900 dark:text-white">Kantor Pusat</h4>
+                    <div class="font-normal text-center text-gray-700 dark:text-gray-400">Dusun Bulusari, Kelurahan Bulusulur, Kecamatan Wonogiri, Kabupaten Wonogiri, Provinsi Jawa Tengah</div>
+                </div>
+    
+                {{-- Contact Us --}}
+                <div class="w-full px-4 mx-auto lg:px-6 py-4">
+                    <dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-2 dark:text-white">
+                        <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold"><i class="bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600 fa-brands fa-whatsapp"></i></dt>
+                        
+                            <dd class="font-normal text-gray-700 dark:text-gray-400">
+                                <a href="https://api.whatsapp.com/send?phone=6287773777005&amp;text=Halo%20Kak,%20Perkenalkan%0ANama%20Saya%20=%20%0AAlamat%20=%20%0AKeperluan%20=%20"> 08777-3777-005 </a>
+                            </dd>
+    
+                            <dd class="font-bold text-gray-700 dark:text-gray-400">WhatsApp Service Center</dd>
+                            
+                        </div>
+                        <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold"><i class="bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600 fa-solid fa-envelope"></i></dt>
+                            <dd class="font-normal text-gray-700 dark:text-gray-400">
+                                <a href="mailto:admin@admin.com"> admin@admin.com </a>
+                            </dd>
+                            <dd class="font-bold text-gray-700 dark:text-gray-400">Email Address</dd>
+                        </div>
+                        {{-- <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold"><i class="bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-indigo-600 fa-solid fa-location-dot"></i></dt>
+                            <dd class="font-normal text-gray-700 dark:text-gray-400">Jl. Raya Ciputat Parung No. 123, Tangerang Selatan</dd>
+                        </div> --}}
+                    </dl>
+    
+                    
+    
+                    {{-- <ul class="list-unstyled mb-0">
+                        <li class="d-flex gap-3"><i class="fas fa-location-arrow company-icon"></i>
+                            <div class="d-flex flex-column gap-2">
+                                <h4 class="mb-1">Kantor Pusat</h4>
+                                <p class="mb-0 opacity-75">Dusun Bulusari, Kelurahan Bulusulur, Kecamatan Wonogiri, Kabupaten Wonogiri, Provinsi Jawa Tengah</p>
+                            </div>
+                        </li>
+                        <li class="d-flex gap-3"><i class="fas fa-envelope company-icon"></i>
+                            <div class="d-flex flex-column gap-2">
+                                <h4 class="mb-0">Email Address</h4><a class="text-dark text-decoration-none" href="mailto:cs@yamnet.id">cs@yamnet.id</a></div>
+                        </li>
+                        <li class="d-flex gap-3"><i class="fab fa-whatsapp company-icon"></i>
+                            <div class="d-flex flex-column gap-2">
+                                <h4 class="mb-0">Whatsapp Service Center</h4><a class="text-dark text-decoration-none" href="https://api.whatsapp.com/send?phone=62811315215&amp;text=Halo%20Kak,%20Perkenalkan%0ANama%20Saya%20=%20%0AAlamat%20=%20%0AKeperluan%20=%20">0811-315-215</a></div>
+                        </li>
+                    </ul> --}}
+    
+                </div>
+            </div>
+
+
+
 
         </div>
     </div>
+
+    <div class="absolute px-5 justify-center items-center w-full -bottom-[175px] md:-bottom-[85px] drop-shadow-xl">
+        <div class="container w-full mx-auto">
+            <div
+                class=" max-w-screen-xl bg-gradient-to-br from-pink-500 to-indigo-600 row justify-center rounded-b-[80px] rounded-t-[20px] px-[30px] pb-8 pt-8">
+                <div class="w-full px-4 mx-auto text-center lg:px-6">
+                    <dl
+                        class="grid gap-4 md:gap-0 max-w-screen-lg mx-auto text-gray-100 grid-cols-2 md:grid-cols-4 dark:text-white md:divide-x-2 divide-gray-100 md:divide-dashed">
+                        <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold">8,113</dt>
+                            <dd class="font-normal text-gray-100 dark:text-gray-400">Happy Clients</dd>
+                        </div>
+                        <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold">12,000</dt>
+                            <dd class="font-normal text-gray-100 dark:text-gray-400">Home Pass</dd>
+                        </div>
+                        <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold">36</dt>
+                            <dd class="font-normal text-gray-100 dark:text-gray-400">Mitra RlNet</dd>
+                        </div>
+                        <div class="flex flex-col items-center justify-center">
+                            <dt class="mb-2 text-3xl md:text-3xl font-bold">205</dt>
+                            <dd class="font-normal text-gray-100 dark:text-gray-400">Project Area</dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 </section>
