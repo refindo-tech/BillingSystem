ea0a3fe5e934e064ab7bef4563b62e143b3d5c84
Hernando Dio Palma <hernando.diopalma@gmail.com>
2024-08-30 22:03:28 +0700

    Update serviceCard component with responsive width

--- resources/views/components/ui/serviceCard.blade.php
+++ resources/views/components/ui/serviceCard.blade.php
@@ -1,4 +1,4 @@
-<div class="max-w-sm p-6 bg-white border border-gray-200 rounded-xl shadow dark:bg-gray-800 dark:border-gray-700">
+<div class="w-full md:max-w-sm p-6 bg-white border border-gray-200 rounded-xl shadow dark:bg-gray-800 dark:border-gray-700">
     <div class="flex justify-center items-center mb-4 w-full h-20 rounded-full bg-primary-100 lg:h-20 lg:w-full dark:bg-primary-900">
         {{-- Icon Slot --}}
         {{ $icon }}
