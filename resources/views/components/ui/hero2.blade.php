af2b63c1234bc32e229521156e3b5ed2e098bbe6
Hernando Dio Palma <hernando.diopalma@gmail.com>
2024-08-30 22:02:37 +0700

    Update package card

--- resources/views/components/ui/hero2.blade.php
+++ resources/views/components/ui/hero2.blade.php
@@ -1,4 +1,4 @@
-<section class="bg-gray-100 dark:bg-gray-900">
+<section class="bg-gray-100 dark:bg-gray-900" id="info">
     <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
         <img class="w-full" src="{{ asset('images/tech3.png') }}" alt="dashboard image">
         {{-- <img class="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image"> --}}
@@ -12,7 +12,7 @@
             <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Untuk internetan di rumah tak perlu mahal. Layanan prima dari YamNet tetap dapat dinikmati sekeluarga dengan pilihan paket sesuai kebutuhan Anda.</p>
 
             <div class="w-full px-4 mx-auto text-center lg:px-6">
-                <dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white divide-x-2 divide-gray-500 divide-dashed">
+                <dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white md:divide-x-2 divide-gray-500 divide-dashed">
                     <div class="flex flex-col items-center justify-center">
                         <dt class="mb-2 text-3xl md:text-3xl font-bold">8,113</dt>
                         <dd class="font-normal text-gray-700 dark:text-gray-400">Happy Clients</dd>
---
+++ resources/views/components/ui/packageCard.blade.php
@@ -0,0 +1,22 @@
+<div class="flex flex-col p-2 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 md:p-6 lg:p-4 xl:p-6 dark:bg-gray-800 dark:text-white">
+    <h3 class="mb-4 text-2xl font-semibold">{{ $title }}</h3>
+    <p class="font-light text-gray-500 sm:text-lg dark:text-gray-400 flex-1">{{ $description }}</p>
+    <div class="flex justify-center items-baseline my-8">
+        <span class="mr-2 text-4xl font-extrabold">Rp.{{ $price }}</span>
+        <span class="text-gray-500 dark:text-gray-400">/bulan</span>
+    </div>
+    <!-- List -->
+    <ul role="list" class="mb-8 space-y-4 text-left flex-1">
+        @foreach ($features as $feature)
+            <li class="flex items-center space-x-1">
+                <!-- Icon -->
+                <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                <span>{{ $feature }}</span>
+            </li>
+        @endforeach
+    </ul>
+   <a href="https://api.whatsapp.com/send?phone=6287773777005&amp;text=Halo%20Kak,%20Perkenalkan%0ANama%20Saya%20=%20%0AAlamat%20=%20%0AKeperluan%20=%20Saya%20ingin%20berlangganan%20paket%20{{ $title }}%20seharga%20Rp.{{ $price }}%20per%20bulan." class="text-white bg-gradient-to-br from-pink-500 to-indigo-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white dark:focus:ring-primary-900">
+    Pesan Sekarang
+    </a>
+    
+</div>
