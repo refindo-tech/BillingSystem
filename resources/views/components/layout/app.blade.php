0ad8ceffbd0115e448131ba5656e7d75bb6436ea
Abdul Majid Refindo <abdulmajirefindo@gmail.com>
2024-09-01 12:37:03 +0700

    fix typo

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
-    <title>{{ $title ?? 'My App' }} | RL.Net</title>
+    {{-- <title>{{ $title ?? 'My App' }} |  NETPLUS Connection</title> --}}
+    <title>NETPLUS Connection | {{ $title ?? 'My App' }}</title>
     @vite('resources/css/tailwind.css')
     @vite('resources/css/app.css')
 </head>
