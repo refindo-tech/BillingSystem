7c9ed4710a5f0f2c6b958bef4156667aadd2fd33
Hernando Dio Palma <hernando.diopalma@gmail.com>
2024-09-01 13:31:24 +0700

    Update UI components for testimony and testimony card

--- resources/views/components/ui/testimony.blade.php
+++ resources/views/components/ui/testimony.blade.php
@@ -27,7 +27,7 @@
         </div>
         <!-- Slider controls -->
         <button type="button"
-            class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            class="absolute top-1/2 pb-16 md:pb-0 md:top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
             data-carousel-prev>
             <span
                 class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60">
@@ -40,7 +40,7 @@
             </span>
         </button>
         <button type="button"
-            class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            class="absolute top-1/2 pb-16 md:pb-0 md:top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
             data-carousel-next>
             <span
                 class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
