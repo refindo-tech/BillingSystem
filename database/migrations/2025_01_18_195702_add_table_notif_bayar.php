2022f03a95520fe91079cff093c2cd8a8bedb9dd
Hernando Dio Palma <hdiopalma@gmail.com>
2025-01-19 03:28:15 +0700

    Add migration for tbl_notifbayar table

---
+++ database/migrations/2025_01_18_195702_add_table_notif_bayar.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_notifbayar', function (Blueprint $table) {
+            $table->integer('id_notifbayar')->primary();
+            $table->text('pesan_bayar');
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_general_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_notifbayar');
+    }
+};
