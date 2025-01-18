83f296ad0ac6c58c2bede2df367f5815ef25824d
Hernando Dio Palma <hdiopalma@gmail.com>
2025-01-19 03:28:21 +0700

    Add migration for tbl_npemasangan table

---
+++ database/migrations/2025_01_18_195723_add_table_npemasangan.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_npemasangan', function (Blueprint $table) {
+            $table->integer('id_npemasangan')->primary();
+            $table->enum('status_notif', ['aktif', 'tidak']);
+            $table->text('pesan_notif');
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_general_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_npemasangan');
+    }
+};
