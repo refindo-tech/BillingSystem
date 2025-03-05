cabf18c09d0934c32306e711c2ab4707f238a86f
Hernando Dio Palma <hdiopalma@gmail.com>
2025-03-05 16:36:48 +0700

    Fix expiration date calculation to handle past dates correctly

--- app/Support/Package.php
+++ app/Support/Package.php
@@ -277,8 +277,7 @@ public static function rechargeUser(
      */
     public static function calculateExpiration(Plan $plan, $validityCycle, $userRecharge)
     {
-        $expiredAt = $userRecharge->expired_at;
-
+        $expiredAt = $userRecharge->expired_at->isPast() ? now() : $userRecharge->expired_at;
         return match ($validityCycle) {
             ValidityCycle::FIXED, ValidityCycle::MONTHLY => date('Y-m-d H:i:s', strtotime($expiredAt. ' + 1 month')),
             default => match ($plan->validity_unit) {
